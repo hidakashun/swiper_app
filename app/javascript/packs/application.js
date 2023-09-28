1d68f47bec067a2a7ce8ca2fb546e6ab199d64cd
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-09-28 08:32:40 +0000

    アラートを表示してみる

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+//アラートを表示してみる
+import "script.js"
 
 Rails.start()
 Turbolinks.start()
---
+++ app/javascript/script.js
@@ -0,0 +1,3 @@
+//アラートを表示してみる
+let title = "javascriptが使えました";
+alert(title);
