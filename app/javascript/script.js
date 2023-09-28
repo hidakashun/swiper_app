8463ca1103493ae2f70edce2b9a4ccf544393257
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-09-28 08:40:35 +0000

    jQueryを使用する_文字の色を変えてみる

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,3 +1,10 @@
 //アラートを表示してみる
 let title = "javascriptが使えました";
 alert(title);
+
+//jQueryを使用する_文字の色を変えてみる
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
